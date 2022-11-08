d2a495f651ec6a6862ca8d5034517f353f51bfb7
Isis_R_Castro <110749924+IsisReisCastro@users.noreply.github.com>
2022-11-08 15:17:28 -0300

    Inserindo Carousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import { useContext } from "react";
 import AuthContext from "../../context/auth";
+import EmblaCarousel from "../../components/Carousel";
 
 export function Home() {
     const { signOut, userName } = useContext(AuthContext);
+    const SLIDE_COUNT = 6;
+    const slides = Array.from(Array(SLIDE_COUNT).keys());
 
     return (
         <div>
-            <h1>Ola, {userName}</h1>
+            <EmblaCarousel slides={slides} />
+            <h1 style={{marginTop:"2rem"}}>Ola, {userName}</h1>
             <button onClick={() => signOut()}>Sair</button>
         </div>
     );
