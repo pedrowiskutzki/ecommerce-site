fad7171f357fd0b3e2b0c5e53d4357428bd2c94f
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2022-11-08 16:52:37 -0300

    feat(Carousel):created carousel

--- src/pages/office.js
+++ src/pages/office.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import produtoService from "../service/request/productService";
+import { Container } from "../pages/styled";
 
 export const Office = () => {
     const [category, setCategory] = useState([]);
@@ -18,47 +19,76 @@ export const Office = () => {
             });
     }, []);
 
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    };
+
+    const handleRightClick = (e) => {
+        e.preventDefault();
+
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    };
+
     if (!category || !category.length) return null;
     const office = 2;
 
     return (
-        <div className="container">
-            <div className="carousel" ref={carousel}>
-                {category.map((item) => {
-                    const {
-                        id,
-                        idCategoria,
-                        nome,
-                        valor,
-                        descricao,
-                        fotoLink,
-                    } = item;
-                    if (idCategoria === office) {
-                        return (
-                            <div className="item" key={id}>
-                                <div className="image">
-                                    <img
-                                        src={fotoLink}
-                                        alt={nome}
-                                        height="400"
-                                    />
+        <Container>
+            <div className="container">
+                <div className="carousel" ref={carousel}>
+                    {category.map((item) => {
+                        const {
+                            id,
+                            idCategoria,
+                            nome,
+                            valor,
+                            descricao,
+                            fotoLink,
+                        } = item;
+                        if (idCategoria === office) {
+                            return (
+                                <div className="item" key={id}>
+                                    <div className="image">
+                                        <img
+                                            src={fotoLink}
+                                            alt={nome}
+                                            height="400"
+                                        />
+                                    </div>
+                                    <div className="info">
+                                        <span className="name">
+                                            Nome: {nome}
+                                        </span>
+                                        <br />
+                                        <span className="descricao">
+                                            Descrição: {descricao}
+                                        </span>
+                                        <br />
+                                        <span className="price">
+                                            U$ Valor: ${valor.toFixed(2)}
+                                        </span>
+                                    </div>
                                 </div>
-                                <div className="info">
-                                    <span className="name">Nome: {nome}</span>
-                                    <br />
-                                    <span className="descricao">
-                                        Descrição: {descricao}
-                                    </span>
-                                    <br />
-                                    <span className="price">
-                                        U$ Valor: ${valor.toFixed(2)}
-                                    </span>
-                                </div>
-                            </div>
-                        );
-                    }
-                })}
+                            );
+                        }
+                    })}
+                </div>
+                <div className="buttons">
+                    <button onClick={handleLeftClick}>
+                        <img
+                            src="/static/images/216151_right_chevron_icon.png"
+                            alt="Scroll Left"
+                        />
+                    </button>
+                    <button onClick={handleRightClick}>
+                        <img
+                            src="/static/images/216151_right_chevron_icon.png"
+                            alt="Scroll Right"
+                        />
+                    </button>
+                </div>
             </div>
-        </div>
+        </Container>
     );
 };
