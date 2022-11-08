cb9500f67744d6978954f4cebe5d2f56185b2e37
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2022-11-08 00:22:06 -0300

    feat(Category):Created Category

--- src/pages/bookstore.js
+++ src/pages/bookstore.js
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
+import { useState } from "react";
+import produtoService from "../service/request/productService";
 
 export const Bookstore = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>Livraria</h1>
-    </div>
-  );
-};
+    const [category, setCategory] = useState([]);
+    const carousel = useRef(null);
 
+    useEffect(() => {
+        produtoService
+            .get(category)
+            .then((res) => {
+                console.log(res.data);
+                setCategory(res.data);
+            })
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }, []);
+    if (!category || !category.length) return null;
+    const books = 3;
+
+    return (
+        <div className="container">
+            <div className="carousel" ref={carousel}>
+                {category.map((item) => {
+                    const {
+                        id,
+                        idCategoria,
+                        nome,
+                        valor,
+                        descricao,
+                        fotoLink,
+                    } = item;
+                    if (idCategoria === books) {
+                        return (
+                            <div className="item" key={id}>
+                                <div className="image">
+                                    <img
+                                        src={fotoLink}
+                                        alt={nome}
+                                        height="400"
+                                    />
+                                </div>
+                                <div className="info">
+                                    <span className="name">Nome: {nome}</span>
+                                    <br />
+                                    <span className="descricao">
+                                        Descrição: {descricao}
+                                    </span>
+                                    <br />
+                                    <span className="price">
+                                        U$ Valor: ${valor.toFixed(2)}
+                                    </span>
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
+            </div>
+        </div>
+    );
+};
--- src/pages/computing.js
+++ src/pages/computing.js
@@ -1,109 +1,63 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import produtoService from "../service/request/productService";
 
 export const Computing = () => {
-    const [product, setProduct] = useState([]);
-    const [escrivaniha, setEscrivaniha] = useState([]);
-    const [inferno, setInferno] = useState([]);
-    const [tablet, setTablet] = useState([]);
-
-    const cadeiraBx9 = 1;
-    const Escrivaniha = 2;
-    const Inferno = 3;
-    const Tablet = 5;
+    const [category, setCategory] = useState([]);
+    const carousel = useRef(null);
 
     useEffect(() => {
         produtoService
-            .get(cadeiraBx9)
+            .get(category)
             .then((res) => {
                 console.log(res.data);
-                setProduct(res.data);
-            })
-            .catch((err) => {
-                console.error("ops! ocorreu um erro" + err);
-            });
-        produtoService
-            .get(Escrivaniha)
-            .then((res) => {
-                console.log(res.data);
-                setEscrivaniha(res.data);
-            })
-            .catch((err) => {
-                console.error("ops! ocorreu um erro" + err);
-            });
-        produtoService
-            .get(Inferno)
-            .then((res) => {
-                console.log(res.data);
-                setInferno(res.data);
-            })
-            .catch((err) => {
-                console.error("ops! ocorreu um erro" + err);
-            });
-        produtoService
-            .get(Tablet)
-            .then((res) => {
-                console.log(res.data);
-                setTablet(res.data);
+                setCategory(res.data);
             })
             .catch((err) => {
                 console.error("ops! ocorreu um erro" + err);
             });
     }, []);
+
+    if (!category || !category.length) return null;
+    const informatica = 1;
+
     return (
-        <div>
-            <div>
-                <img
-                    src={product.fotoLink}
-                    alt={`image_${product.descricao}`}
-                    height="400"
-                />
-                <p>{`Nome: ${product.nome}`}</p>
-                <p>{`Descrição: ${product.descricao}`}</p>
-                <p>{`Valor: ${product.valor},00`}</p>
-                <p>{`Categoria: ${product.nomeCategoria}`}</p>
-                <p>{`Funcionario: ${product.nomeFuncionario}`}</p>
-                <p>{`Quantidade em estoque: ${product.qtdEstoque}`}</p>
-            </div>
-            <div>
-                <img
-                    src={escrivaniha.fotoLink}
-                    alt={`image_${escrivaniha.descricao}`}
-                    height="400"
-                />
-                <p>{`Nome: ${escrivaniha.nome}`}</p>
-                <p>{`Descrição: ${escrivaniha.descricao}`}</p>
-                <p>{`Valor: ${escrivaniha.valor},00`}</p>
-                <p>{`Categoria: ${escrivaniha.nomeCategoria}`}</p>
-                <p>{`Funcionario: ${escrivaniha.nomeFuncionario}`}</p>
-                <p>{`Quantidade em estoque: ${escrivaniha.qtdEstoque}`}</p>
-            </div>
-            <div>
-                <img
-                    src={inferno.fotoLink}
-                    alt={`image_${inferno.descricao}`}
-                    height="400"
-                />
-                <p>{`Nome: ${inferno.nome}`}</p>
-                <p>{`Descrição: ${inferno.descricao}`}</p>
-                <p>{`Valor: ${inferno.valor},00`}</p>
-                <p>{`Categoria: ${inferno.nomeCategoria}`}</p>
-                <p>{`Funcionario: ${inferno.nomeFuncionario}`}</p>
-                <p>{`Quantidade em estoque: ${inferno.qtdEstoque}`}</p>
-            </div>
-            <div>
-                <img
-                    src={tablet.fotoLink}
-                    alt={`image_${tablet.descricao}`}
-                    height="400"
-                />
-                <p>{`Nome: ${tablet.nome}`}</p>
-                <p>{`Descrição: ${tablet.descricao}`}</p>
-                <p>{`Valor: ${tablet.valor},00`}</p>
-                <p>{`Categoria: ${tablet.nomeCategoria}`}</p>
-                <p>{`Funcionario: ${tablet.nomeFuncionario}`}</p>
-                <p>{`Quantidade em estoque: ${tablet.qtdEstoque}`}</p>
+        <div className="container">
+            <div className="carousel" ref={carousel}>
+                {category.map((item) => {
+                    const {
+                        id,
+                        idCategoria,
+                        nome,
+                        valor,
+                        descricao,
+                        fotoLink,
+                    } = item;
+                    if (idCategoria === informatica) {
+                        return (
+                            <div className="item" key={id}>
+                                <div className="image">
+                                    <img
+                                        src={fotoLink}
+                                        alt={nome}
+                                        height="400"
+                                    />
+                                </div>
+                                <div className="info">
+                                    <span className="name">Nome: {nome}</span>
+                                    <br />
+                                    <span className="descricao">
+                                        Descrição: {descricao}
+                                    </span>
+                                    <br />
+                                    <span className="price">
+                                        U$ Valor: ${valor.toFixed(2)}
+                                    </span>
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
             </div>
         </div>
     );
